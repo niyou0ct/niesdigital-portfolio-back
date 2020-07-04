d1fafa2e6bdac5c5d016bca9deaa4af03b00161e
niyou0ct <axaebyxo11@gmail.com>
2020-07-04 14:02:53 +0900

    update: setting email

--- send_email.py
+++ send_email.py
@@ -1,20 +1,21 @@
 from email.mime.text import MIMEText
-import smtplib
+import smtplib, ssl
 
-account = 'sample@gmail.com'
-password = '******'
+account = 'yuki.niiuchi@niesdigital.com'
+password = 'Yn170409'
 
 to_email = account
-from_email = ''
+from_email = 'axaebyxo11@gmail.com'
 
 subject = 'テストメール'
 message = 'テストメール'
 msg = MIMEText(message, 'html')
+msg['Subject'] = subject
 msg['To'] = to_email
 msg['From'] = from_email
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
+# server = smtplib.SMTP_SSL('smtp.gmail.com', 465, context = ssl.create_default_context())
+server = smtplib.SMTP('smtp.gmail.com', 465)
 server.login(account, password)
 server.send_message(msg)
 server.quit()
